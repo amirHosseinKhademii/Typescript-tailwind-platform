42784d1f500b64d712d04fa4c0e151f64793aa06
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-06-27 21:29:10 +0430

    WEBPACK: Implement webpack config

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "../src/app/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "../src/app/index.html"),
+    }),
+  ],
+};
